125cb2473be9ccf1aafc2fd191b3c1ce149edbc8
Simone Oprandi <simone.oprandi@cgm.com>
2021-05-19 17:24:41 +0200

    switch to HTTP

--- api/src/server.js
+++ api/src/server.js
@@ -1,9 +1,18 @@
-import { graphql } from 'graphql';
-import { schema, rootValue } from '../schema'
+import { graphqlHTTP } from 'express-graphql';
+import * as config from '../config';
+import express from 'express';
+import { schema, rootValue } from '../schema';
 
-const executeGraphQLRequest = async request => {
-const resp = await graphql(schema, request, rootValue);
-console.log(resp.data);
-};
-
-executeGraphQLRequest(process.argv[2]);
+async function main() {
+    const server = express();
+    server.use('/',
+            graphqlHTTP({
+                schema,
+                rootValue,
+                graphiql: true,
+            }));
+    server.listen(config.port, () => {
+        console.log(`Server URL: http://localhost:${config.port}/`);
+        });
+}
+main();
