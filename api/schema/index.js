135ebe9dff3a6bb91907a29a2b6b11e7cdaa9ce8
Simone Oprandi <simone.oprandi@cgm.com>
2021-05-20 17:33:32 +0200

    Revert "context object dbclient"
    This reverts commit 74797459e01eb39bb2fe2c008944a364058c70fa.

--- api/schema/index.js
+++ api/schema/index.js
@@ -1,6 +1,11 @@
 import { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLList } from 'graphql';
 import { BookType } from '../model/book';
+import * as config from '../config';
 const books = require('../data/books.json')
+
+const pgp = require('pg-promise')();
+const db = {}
+db.conn = pgp(config.connectionString);
 
 const QueryType = new GraphQLObjectType({
     name: 'Query',
@@ -13,18 +18,18 @@ const QueryType = new GraphQLObjectType({
             }
         },
         bookList: {
-            type: new GraphQLList( BookType ),
+            type: new GraphQLList( BookType),
             description: 'API che restituisce i libri nel file statico json',
             resolve: ()=> {
                 return books;
             }
         },
-         booksList2: {
+        booksList2: {
             type: new GraphQLList(BookType),
             description: ".....",
-            resolve: (source, args, { dbclient }) => {
+            resolve: () => {
                 const query = `SELECT * FROM example.book`;
-                return dbclient.conn.many(query)
+                return db.conn.many(query)
                    .then(data => { return data; })
                    .catch(err => { return 'The error is', err; });
             }
