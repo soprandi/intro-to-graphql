74797459e01eb39bb2fe2c008944a364058c70fa
Simone Oprandi <simone.oprandi@cgm.com>
2021-05-20 17:30:59 +0200

    context object dbclient

--- api/schema/index.js
+++ api/schema/index.js
@@ -1,11 +1,6 @@
 import { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLList } from 'graphql';
 import { BookType } from '../model/book';
-import * as config from '../config';
 const books = require('../data/books.json')
-
-const pgp = require('pg-promise')();
-const db = {}
-db.conn = pgp(config.connectionString);
 
 const QueryType = new GraphQLObjectType({
     name: 'Query',
@@ -18,18 +13,18 @@ const QueryType = new GraphQLObjectType({
             }
         },
         bookList: {
-            type: new GraphQLList( BookType),
+            type: new GraphQLList( BookType ),
             description: 'API che restituisce i libri nel file statico json',
             resolve: ()=> {
                 return books;
             }
         },
-        booksList2: {
+         booksList2: {
             type: new GraphQLList(BookType),
             description: ".....",
-            resolve: () => {
+            resolve: (source, args, { dbclient }) => {
                 const query = `SELECT * FROM example.book`;
-                return db.conn.many(query)
+                return dbclient.conn.many(query)
                    .then(data => { return data; })
                    .catch(err => { return 'The error is', err; });
             }
--- api/src/server.js
+++ api/src/server.js
@@ -2,14 +2,16 @@ import { graphqlHTTP } from 'express-graphql';
 import * as config from '../config';
 import express from 'express';
 import { schema } from '../schema';
+import { dbclient } from './db/db-client';
 
 async function main() {
     const server = express();
     server.use('/',
             graphqlHTTP({
                 schema,
-              //  rootValue,
                 graphiql: true,
+                context: { dbclient }
+              
             }));
     server.listen(config.port, () => {
         console.log(`Server URL: http://localhost:${config.port}/`);
