6071143ab5015ddb4330556068d4c77f357d3f2a
Simone Oprandi <simone.oprandi@cgm.com>
2021-05-21 18:00:24 +0200

    enabled cors

--- api/src/server.js
+++ api/src/server.js
@@ -3,9 +3,12 @@ import * as config from '../config';
 import express from 'express';
 import { schema } from '../schema';
 import { dbclient } from './db/db-client';
+var cors = require('cors')
 
 async function main() {
     const server = express();
+    server.use(cors());
+
     server.use('/',
             graphqlHTTP({
                 schema,
